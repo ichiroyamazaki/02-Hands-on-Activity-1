c130924b40ce2e81cc72ab409992a2b00173bb80
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-06-25 12:00:12 +0800

    Add files via upload

---
+++ NumberAlgorithm.py
@@ -0,0 +1,12 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 < num2:
+    print("The first number is less than the second number.")
+elif num1 > num2:
+    print("The first number is greater than the second number.")
+else:
+    print("The first number is equal to the second number.")
